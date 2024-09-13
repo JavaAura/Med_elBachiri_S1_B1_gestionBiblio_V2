f696060132a448fd56d39c7793f225a7d414e21f
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-13 22:52:05 +0100

    Update ScientificJournalDaoImpl.java

--- app/src/main/java/com/library/daoImpl/ScientificJournalDaoImpl.java
+++ app/src/main/java/com/library/daoImpl/ScientificJournalDaoImpl.java
@@ -30,6 +30,7 @@ public class ScientificJournalDaoImpl implements ScientificJournalDAO {
                         resultSet.getInt("impact_factor")
                 );
                 journal.setId(resultSet.getString("id"));
+                journal.setBorrowed(resultSet.getBoolean("borrowed"));
                 journals.put("SJ" + journal.getId(), journal);
             }
         } catch (SQLException e) {
@@ -45,13 +46,16 @@ public class ScientificJournalDaoImpl implements ScientificJournalDAO {
             preparedStatement.setString(1, id);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
-                    return new ScientificJournal(
+                    ScientificJournal scientificJournal = new ScientificJournal(
                             resultSet.getString("title"),
                             resultSet.getString("author"),
                             resultSet.getDate("pub_date").toLocalDate(),
                             resultSet.getInt("num_pages"),
                             resultSet.getInt("impact_factor")
                     );
+                    scientificJournal.setBorrowed(resultSet.getBoolean("borrowed"));
+                    scientificJournal.setId(String.valueOf(resultSet.getInt("id")));
+                    return scientificJournal;
                 }
             }
         } catch (SQLException e) {
@@ -90,14 +94,15 @@ public class ScientificJournalDaoImpl implements ScientificJournalDAO {
 
     @Override
     public void update(ScientificJournal scJou) {
-        String query = "UPDATE scientific_journals SET title = ?, author = ?, pub_date = ?, num_pages = ? WHERE id = ?";
+        String query = "UPDATE scientific_journals SET title = ?, author = ?, pub_date = ?, num_pages = ?, impact_factor = ?, borrowed = ? WHERE id = ?";
         try (PreparedStatement prestm = cn.prepareStatement(query)) {
             prestm.setString(1, scJou.getTitle());
             prestm.setString(2, scJou.getAuthor());
             prestm.setDate(3, Date.valueOf(scJou.getPubDate()));
             prestm.setInt(4, scJou.getNumPages());
             prestm.setDouble(5, scJou.getImpactFactor());
-            prestm.setString(6, scJou.getId());
+            prestm.setBoolean(6, scJou.getBorrowed());
+            prestm.setString(7, scJou.getId());
             prestm.executeUpdate();
             System.out.println("[+] Scientific Journal updated.");
         } catch (SQLException e) {
