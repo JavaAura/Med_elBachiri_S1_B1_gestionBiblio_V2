0d94ab0fa5e1ad4e32dc1e29e8ad68d6a049eef5
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-14 18:43:05 +0100

    Update ReservationMenu.java

--- app/src/main/java/com/library/ui/ReservationMenu.java
+++ app/src/main/java/com/library/ui/ReservationMenu.java
@@ -2,6 +2,10 @@ package com.library.ui;
 
 public class ReservationMenu {
     public static void display(){
-
+        System.out.println("===== Reservation Managment =====");
+        System.out.println("1. View Borrowed Documents");
+        System.out.println("2. Manage Users");
+        System.out.println("3. Manage Reservations");
+        System.out.println("4. Exit");
     }
 }
